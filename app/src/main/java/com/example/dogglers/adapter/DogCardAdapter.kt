0045511fcb56f75dc5fb5aa0fe8416bc6c197131
Zheng Wei <65906926+de-only-wei@users.noreply.github.com>
2023-02-08 07:30:56 -0800

    subtle changes

--- app/src/main/java/com/example/dogglers/adapter/DogCardAdapter.kt
+++ app/src/main/java/com/example/dogglers/adapter/DogCardAdapter.kt
@@ -37,10 +37,13 @@ class DogCardAdapter(
 
 ): RecyclerView.Adapter<DogCardAdapter.DogCardViewHolder>() {
     // TODO: Initialize the data using the List found in data/DataSource
-    val dogs = DataSource.dogs
+    private val dogs = DataSource.dogs
     class DogCardViewHolder(view: View?): RecyclerView.ViewHolder(view!!) {
         // TODO: Declare and initialize all of the list item UI components
-
+        val name: TextView = itemView.findViewById(R.id.dog_name)
+        val age: TextView = itemView.findViewById(R.id.dog_age)
+        val hobbies: TextView = itemView.findViewById(R.id.dog_hobbies)
+        val image: ImageView = view!!.findViewById(R.id.image)
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): DogCardViewHolder {
@@ -55,17 +58,26 @@ class DogCardAdapter(
         return DogCardViewHolder(null)
     }
 
-    override fun getItemCount(): Int = 0 // TODO: return the size of the data set instead of 0
+    override fun getItemCount(): Int = dogs.size // TODO: return the size of the data set instead of 0
 
     override fun onBindViewHolder(holder: DogCardViewHolder, position: Int) {
         // TODO: Get the data at the current position
+        val item = dogs[position]
         // TODO: Set the image resource for the current dog
+        holder.image.setImageResource(item.imageResourceId)
+
         // TODO: Set the text for the current dog's name
+        holder.name.text = context.resources.getString(item.stringResourceId)
+
         // TODO: Set the text for the current dog's age
+
         val resources = context?.resources
         // TODO: Set the text for the current dog's hobbies by passing the hobbies to the
         //  R.string.dog_hobbies string constant.
         //  Passing an argument to the string resource looks like:
         //  resources?.getString(R.string.dog_hobbies, dog.hobbies)
+        holder.name.text = resources?.getString(R.string.dog_age)
+        holder.age.text
+        holder.hobbies.text
     }
 }
